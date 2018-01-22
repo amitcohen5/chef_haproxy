c038c37c0da59f54c5a538fd41d4b1203bc4604f
root <root@amitcohen51.mylabserver.com>
2018-01-22 17:03:26 +0000

    edit haproxy template and recipe to add nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+node.default['haproxy']['members'] = [
+  {
+  "hostname" => "amitcohen51",
+  "ipaddress" => "172.31.117.187",
+  "port" => 80,
+  "ssl_port" => 80
+},{
+  "hostname" => "amitcohen52",
+  "ipaddress" => "172.31.47.200",
+  "port" => 80,
+  "ssl_port" => 80
+}]
+
+include_recipe "haproxy::manual"
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  action :create
+end
+
+
